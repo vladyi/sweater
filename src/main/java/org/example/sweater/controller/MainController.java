76969990d55913bb94f5ec9ae7830201dee655e6
vladyi <gumenskyi@gmail.com>
2021-01-23 23:15:55 +0200

    Add links between tables DB

--- src/main/java/org/example/sweater/controller/MainController.java
+++ src/main/java/org/example/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package org.example.sweater.controller;
 
 import org.example.sweater.domain.Message;
+import org.example.sweater.domain.User;
 import org.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,8 +33,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
