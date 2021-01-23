c4914543425f8b9fa38d6ff816ae207acfc1364e
vladyi <gumenskyi@gmail.com>
2021-01-23 22:18:57 +0200

    Add study project sweater

---
+++ src/main/java/org/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package org.example.sweater.repos;
+
+import org.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
---
+++ src/main/java/org/example/sweater/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package org.example.sweater.repos;
+
+import org.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
